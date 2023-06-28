99c648a18b34074cdf13312e951ce20f8c9257b2
Bobolan123 <127038290+Bobolan123@users.noreply.github.com>
2023-06-28 20:03:43 +0700

    delete all files

--- tasks/views.py
+++ tasks/views.py
@@ -7,25 +7,27 @@ from django.urls import reverse
 
 class NewTaskForm(forms.Form):
     task = forms.CharField(label="newtask")
-    priority = forms.IntegerField(label="priority", max_value=10, min_value=1)
+    priority = forms.IntegerField(label="priority", max_value=20, min_value=1)
 
-def index(request):     
+def index(request):
     if "tasks" not in request.session:
         request.session["tasks"] = []
-
     if request.method == "POST" and "delete" in request.POST:
         request.session["tasks"] = []
         return HttpResponseRedirect(reverse("tasks:index"))
     return render(request, "tasks/index.html", {
-        "tasks" : request.session["tasks"]
+        "form" : request.session["tasks"]
     })
      
 def add(request):
     if request.method == "POST":
         form = NewTaskForm(request.POST) 
-        if form.is_valid():
+        priority = NewTaskForm(request.POST)
+        if form.is_valid() and priority.is_valid():
             task = form.cleaned_data["task"] # square bracket
-            request.session["tasks"] += [task]
+            priority = form.cleaned_data["priority"]
+            new_task = {"task": task, "priority": priority}
+            request.session["tasks"].append(new_task)
             return HttpResponseRedirect(reverse("tasks:index"))
         else:
             return render(request, "tasks/add.html", {
